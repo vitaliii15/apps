e6ebc988c6f8119ee6b12602d2b848137c49e77d
vitalii15 <v.ariefiev@gmail.com>
2024-10-20 17:22:18 +0300

    changed

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,4 @@
 // import spamFilter from './modules/spam-filter.js';
 // import musicPlayer from './modules/music-player.js';
 // import kyu_8 from './modules/8kyu-codeWars.js';
+// import kyu_8 from './modules/fcc-forum-leaderboard.js';
